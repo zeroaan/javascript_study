8da82941e227581c4184779f09fa21aead6753e5
Jinyoung Ahn <yj6210ag@gmail.com>
2020-08-05 16:18:00 +0900

    file upload

---
+++ js_study/200628/sort.js
@@ -0,0 +1,12 @@
+var numbers = [20, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+var sortfunc = function(a, b){
+    console.log(a, b);
+    if(a > b){
+        return 1;
+    } else if(a < b){
+        return -1;
+    } else {
+        return 0;
+    }
+}
+console.log(numbers.sort(sortfunc));
