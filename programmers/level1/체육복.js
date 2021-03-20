2936fedf5e1f7e8c607430a0c1c09e51868cf09d
zeroaan <yj6210ag@gmail.com>
2021-03-20 14:54:35 +0900

    Update: 2021.03.20. 프로그래머스 문제 다시 풀이
    체육복 문제 다른 사람 풀이 참조

--- programmers/level1/체육복.js
+++ programmers/level1/체육복.js
@@ -40,3 +40,14 @@ function solution(n, lost, reserve) {
   }
   return answer
 }
+
+/*
+function solution(n, lost, reserve) {      
+    return n - lost.filter(a => {
+        const b = reserve.find(r => Math.abs(r-a) <= 1)
+        if(!b) return true
+        reserve = reserve.filter(r => r !== b)
+    }).length
+}
+
+*/
