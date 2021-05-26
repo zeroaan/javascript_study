ec6317d8a7e874aaee44fc2e138c42eb1634dcd6
zeroaan <yj6210ag@gmail.com>
2021-05-26 21:17:00 +0900

    Update: 2021.05.25. 프로그래머스 문제 풀이
    구명보트

--- programmers/level2/구명보트.js
+++ programmers/level2/구명보트.js
@@ -12,20 +12,13 @@
 */
 
 function solution(people, limit) {
-  const save = []
+  let answer = 0
+  people.sort((a, b) => b - a)
 
   for (let i = 0; i < people.length; i++) {
-    if (save.includes(i)) continue
-
-    let value = people[i]
-
-    for (let j = i + 1; j < people.length; j++) {
-      if (value + people[j] <= limit) {
-        value += people[j]
-        save.push(j)
-      }
-    }
+    people[i] + people[people.length - 1] <= limit && people.pop()
+    answer++
   }
 
-  return people.length - save.length
+  return answer
 }
