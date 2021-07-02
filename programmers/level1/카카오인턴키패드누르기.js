f75236300ada3b02b68994cc0c3af3412556c0fc
zeroaan <yj6210ag@gmail.com>
2021-07-02 15:25:07 +0900

    Update: 2021.07.02. 프로그래머스 재풀이
    키패드 누르기

--- programmers/level1/카카오인턴키패드누르기.js
+++ programmers/level1/카카오인턴키패드누르기.js
@@ -16,43 +16,105 @@
 */
 
 function solution(numbers, hand) {
-  const keyboard = [
-    [1, 4, 7, "*"],
-    [2, 5, 8, 0],
-    [3, 6, 9, "#"],
-  ]
-  const current = { L: [0, 3], R: [2, 3] }
+  let answer = "";
 
-  const result = numbers.map((num) => {
-    console.log(current)
-    if (keyboard[0].includes(num)) {
-      current.L = [0, (num - 1) / 3]
-      return "L"
-    } else if (keyboard[2].includes(num)) {
-      current.R = [2, (num - 3) / 3]
-      return "R"
-    } else if (keyboard[1].includes(num)) {
-      const curKey = [1, num === 0 ? 3 : (num - 2) / 3]
-      const leftLength = Math.abs(curKey[0] - current.L[0]) + Math.abs(curKey[1] - current.L[1])
-      const rightLength = Math.abs(curKey[0] - current.R[0]) + Math.abs(curKey[1] - current.R[1])
+  let leftHand = "*";
+  let rightHand = "#";
 
-      if (leftLength < rightLength) {
-        current.L = [1, num === 0 ? 3 : (num - 2) / 3]
-        return "L"
-      } else if (leftLength > rightLength) {
-        current.R = [1, num === 0 ? 3 : (num - 2) / 3]
-        return "R"
-      } else {
+  numbers.forEach((num) => {
+    if (num === 1 || num === 4 || num === 7) {
+      answer += "L";
+      leftHand = num;
+    } else if (num === 3 || num === 6 || num === 9) {
+      answer += "R";
+      rightHand = num;
+    } else {
+      const leftDistance = checkDistance(leftHand, num);
+      const rightDistance = checkDistance(rightHand, num);
+
+      if (leftDistance === rightDistance) {
         if (hand === "left") {
-          current.L = [1, num === 0 ? 3 : (num - 2) / 3]
-          return "L"
-        } else if (hand === "right") {
-          current.R = [1, num === 0 ? 3 : (num - 2) / 3]
-          return "R"
+          answer += "L";
+          leftHand = num;
+        } else {
+          answer += "R";
+          rightHand = num;
         }
+      } else if (leftDistance < rightDistance) {
+        answer += "L";
+        leftHand = num;
+      } else {
+        answer += "R";
+        rightHand = num;
       }
     }
-  })
+  });
 
+  return answer;
+}
+
+function checkDistance(curNumber, point) {
+  const keypad = {
+    1: [0, 0],
+    2: [0, 1],
+    3: [0, 2],
+    4: [1, 0],
+    5: [1, 1],
+    6: [1, 2],
+    7: [2, 0],
+    8: [2, 1],
+    9: [2, 2],
+    "*": [3, 0],
+    0: [3, 1],
+    "#": [3, 2],
+  };
+
+  const [curX, curY] = keypad[curNumber];
+  const [pointX, pointY] = keypad[point];
+
+  return Math.abs(curX - pointX) + Math.abs(curY - pointY);
+}
+
+/*
+function solution(numbers, hand) {
+  const keyboard = [
+      [1, 4, 7, "*"],
+      [2, 5, 8, 0],
+      [3, 6, 9, "#"],
+  ]
+  const current = { L: [0, 3], R: [2, 3] }
+  
+  const result = numbers.map((num) => {
+      console.log(current)
+      if (keyboard[0].includes(num)) {
+          current.L = [0, (num - 1) / 3]
+          return "L"
+      } else if (keyboard[2].includes(num)) {
+          current.R = [2, (num - 3) / 3]
+          return "R"
+      } else if (keyboard[1].includes(num)) {
+          const curKey = [1, num === 0 ? 3 : (num - 2) / 3]
+          const leftLength = Math.abs(curKey[0] - current.L[0]) + Math.abs(curKey[1] - current.L[1])
+          const rightLength = Math.abs(curKey[0] - current.R[0]) + Math.abs(curKey[1] - current.R[1])
+          
+          if (leftLength < rightLength) {
+              current.L = [1, num === 0 ? 3 : (num - 2) / 3]
+              return "L"
+          } else if (leftLength > rightLength) {
+              current.R = [1, num === 0 ? 3 : (num - 2) / 3]
+              return "R"
+          } else {
+              if (hand === "left") {
+                  current.L = [1, num === 0 ? 3 : (num - 2) / 3]
+                  return "L"
+              } else if (hand === "right") {
+                  current.R = [1, num === 0 ? 3 : (num - 2) / 3]
+                  return "R"
+              }
+          }
+      }
+  })
+  
   return result.join("")
 }
+*/
